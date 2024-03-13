9d62918c49da7d593ddda9bf29c4a4c8e70426f4
Claudemar Leal <89711265+clauderleal@users.noreply.github.com>
2024-03-13 16:28:19 +0000

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+# install pm2 which ins a production process manager for node
+sudo npm install -g pm2
+# stop any instance of our app running currently
+pm2 stop simpleapp
+# change directory into folder where application is downloaded
+cd simpleapp
+# install application dependencies
+npm install
+npm install truffle
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+# start application
+pm2 start ./bin/www --name simpleapp
